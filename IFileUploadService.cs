d5d56f1699793b8463544d232f2bda2802488bf6
MohamedYehiaC <74696430+MohamedYehiaC@users.noreply.github.com>
2021-10-03 10:42:36 +0200

    Add files via upload

---
+++ IFileUploadService.cs
@@ -0,0 +1,17 @@
+﻿using App.Core.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Core.Interfaces.Services
+{
+    public interface IFileUploadService
+    {
+        
+        public void FolderCreate(string path);
+
+        public List<FileUploadModel> PostFile(IFormFileCollection uploadedFiles , string folderName);
+        string RemoveSpecialCharachter(string folderName);
+    }
+}
